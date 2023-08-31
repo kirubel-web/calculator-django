c63f0510f3a721a79cb58765523dda946dc866b5
kirubel-web <kirubelalemu119@gmail.com>
2023-08-31 23:55:41 +0300

    third commit

--- calculatorapp/views.py
+++ calculatorapp/views.py
@@ -5,4 +5,16 @@ def home(request):
 
 def result(request):
     num1 = int(request.GET.get('number1'))
+    num2 = int(request.GET.get('number2'))
+
+    if request.GET.get('add') == "":
+        ans = num1 + num2
+    elif request.GET.get('substract') == "":
+        ans = num1 - num2
+    elif request.GET.get('multiply') == "":
+        ans = num1 * num2
+    else:
+        ans = num1 / num2
+
+    return render(request, 'result.html', {'ans': ans})
 # Create your views here.
