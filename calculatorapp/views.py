399446a3744300a6250b73f00ed00aaf9a56c7eb
kirubel-web <kirubelalemu119@gmail.com>
2023-08-31 23:50:07 +0300

    second commit: urls: added result, views: working on pythonish code, templete with cool graphics.

--- calculatorapp/views.py
+++ calculatorapp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def result(request):
+    num1 = int(request.GET.get('number1'))
 # Create your views here.
